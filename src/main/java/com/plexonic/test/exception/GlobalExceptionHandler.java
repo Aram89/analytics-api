4219457c4a7346226a02f7775a34f59e2ecc0639
Aram89 <aramkirakosyan89@gmail.com>
2017-07-19 12:14:45 +0400

    Added comments.

--- src/main/java/com/plexonic/test/exception/GlobalExceptionHandler.java
+++ src/main/java/com/plexonic/test/exception/GlobalExceptionHandler.java
@@ -7,24 +7,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
+ * This class is used for hanling and translating all exceptions.  
+ *
  * @author Aram Kirakosyan.
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    /**
-     * Returning bad request status
-     * and error string for user exceptions
-     * @param e Exception.
-     * @return error string in json.
-     */
-
+ 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public String  userException (Exception e) {
         return e.getMessage();
     }
-
-
 }
