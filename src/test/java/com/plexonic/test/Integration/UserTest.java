9cf99624d2920914890aef72c395d64eb39f6c53
Aram89 <aramkirakosyan89@gmail.com>
2017-07-19 12:19:26 +0400

    Added some formatting.

--- src/test/java/com/plexonic/test/Integration/UserTest.java
+++ src/test/java/com/plexonic/test/Integration/UserTest.java
@@ -10,11 +10,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 /**
  * @author Aram Kirakosyan
@@ -28,6 +25,7 @@ public class UserTest extends BaseTest{
             @Sql(scripts = "/sql/clean-db.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     })
     public void calculateDAUTest() throws Exception {
+        // Make a request.
         MvcResult result  = mockMvc.perform(get("/dau?dates=21/06/2017,20/06/2017"))
                 .andExpect(status().isOk()).andReturn();
 
@@ -46,10 +44,11 @@ public class UserTest extends BaseTest{
             ({@Sql("/sql/test.sql"),@Sql(scripts = "/sql/clean-db.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
             })
     public void calculateRetentionTest() throws Exception {
+        // Make a request.
         MvcResult result  = mockMvc.perform(get("/retention?type=DAY1&date=20/06/2017"))
                 .andExpect(status().isOk()).andReturn();
 
-        // Get response body in json String
+        // Get response body in String
         String content = result.getResponse().getContentAsString();
         assertEquals("50.0", content);
     }
